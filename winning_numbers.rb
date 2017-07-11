6ccc2fae3d73446de59a3791999791c821f30e5a
jkeiffer1 <saros82@hotmail.com>
2017-07-11 18:42:40 -0400

    winning_numbers not working: examining other code. pushing to show progress

--- winning_numbers.rb
+++ winning_numbers.rb
@@ -7,14 +7,53 @@ def lottery(your_ticket,winning_numbers)
 			matches << your_ticket
 		end
 	end
-	matches
+	matches 
 end
 
 
 
-def close_match(your_ticket,winning_numbers)
-	if your_ticket +- 1 == winning_numbers
-		matches << your_ticket
-	end
-	matches
-end
+
+
+
+
+
+
+##############################################examined patricks code to see how it worked #####################
+
+ # def digits_off(your_ticket,winning_numbers)
+ # 	digits_off = 0
+ # 	your_ticket.to_s.split(//).zip(winning_numbers.to_s.split(//)).each {|i| digits_off += 1 if i[0] != i[1]}
+ # 	digits_off == 1
+ # end
+
+ # # 0 mismatching characters..
+ # print "1234 and 1834 differ by one digit: ", digits_off(1234, 1234), "\n\n"
+
+ # # 1 mismatching character
+ # print "1234 and 1235 differ by one digit: ", digits_off(1234,1235), "\n\n"
+
+ # # 2 mismatching characters
+ # print "1234 and 1245 differ by one digit: ", digits_off(1234, 1245), "\n\n"
+
+
+
+
+################################################# first attempt on my own ###############################
+# def close_match(your_ticket,winning_numbers)
+# 	matches = []
+# 	winning_numbers = line.split('')
+# 	winning_numbers.each do |winning_numbers|
+# 		if winning_numbers != ' ' then
+# 			if not count[winning_numbers.to_i] then
+# 				count[winning_numbers.to_i] = 0
+# 			end
+# 			count[winning_numbers.to_i] += 1
+# 			p "#{winning_numbers} matches here"
+# 		end
+# 	end
+# end
+
+#{}"1234"
+
+#{}"5234"
+#{}"1264"
