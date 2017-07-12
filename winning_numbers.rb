d60256232903a72f36898d8da7c1edbfa9fa6fce
jkeiffer1 <saros82@hotmail.com>
2017-07-12 06:57:45 -0400

    added descriptions for each line of code.

--- winning_numbers.rb
+++ winning_numbers.rb
@@ -1,36 +1,36 @@
 def grandbash(ticket,winning_ticket)
-    matches = []
+    matches = []  # creates array for matched numbers
     
    winning_ticket.each do |winner|
-        if  winner == ticket
-            matches << ticket
+        if  winner == ticket # if winner # matches ticket #
+            matches << ticket # pushes into array
         end
     end    
 
-   unless matches.any?
-        one_off?(ticket,winning_ticket)
+   unless matches.any? # if no numbers in the arrays match (false) does one_off? function
+        one_off?(ticket,winning_ticket) # uses one_off function and array
     else
-        matches
+        matches # does matches
     end
 end
 
 
-def one_off?(ticket,winner)
+def one_off?(ticket,winner) #funciton for one digit off
     
-   one_off_arr = []
+   one_off_arr = [] # creates array to be filled
 
-   unless ticket.empty?
+   unless ticket.empty? # returns true if array contains no elements
         
-       ticket_arr = ticket.chars
+       ticket_arr = ticket.chars # splits the ticket numbers and places into an array
 
            
-       winner.each do |win|
-            one_off_counter = 0
+       winner.each do |win| # for each iteration of one off
+            one_off_counter = 0 # sets counter
             
-           winner_arr = win.chars
+           winner_arr = win.chars # splits winning numbers for comparison to ticket numbers and places them into an array
 
 
-               unless winner_arr[0] == ticket_arr[0]
+               unless winner_arr[0] == ticket_arr[0] # if no matches then increase the counter to the next digit
                     one_off_counter += 1
                 end
 
@@ -46,8 +46,8 @@ def one_off?(ticket,winner)
                     one_off_counter += 1
                 end
             
-           if one_off_counter <= 1
-            one_off_arr << win
+           if one_off_counter <= 1 # sets counter to decide when a number that is only one off is chosen as a winner
+            one_off_arr << win # pushes win into one_off_arr array
             end
         end
     end    
